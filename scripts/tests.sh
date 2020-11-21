3335e62133b578874e0bd141d572afdefa84587d
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-11-21 23:04:17 +0000

    Update tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
-ssh -i /home/jenkins/.ssh/id_rsa ubuntu@10.0.1.18
+ssh -i /home/jenkins/.ssh/id_rsa ubuntu@10.0.1.18 << EOF
 
 echo "TEST START"
+
+cd /home/ubuntu/
 
 touch file
 
 echo "TEST END"
 
-exit
+EOF
 
 
 
