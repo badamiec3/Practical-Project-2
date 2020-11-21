1d5ce8c4b4784b0128071f0fd9d8769d64a8751a
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-11-21 23:08:30 +0000

    Update tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,9 +2,13 @@
 
 ssh -i /home/jenkins/.ssh/id_rsa ubuntu@10.0.1.18 << EOF
 
-echo "TEST START"
-
 cd /home/ubuntu/
+
+sudo rm -r Jenkins-Repo-Practical-Project
+
+git clone https://github.com/badamiec3/Jenkins-Repo-Practical-Project.git
+
+cd /home/ubuntu/Jenkins-Repo-Practical-Project
 
 touch file
 
