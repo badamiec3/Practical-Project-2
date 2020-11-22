5a86497f3767756f137d386941c17e75b4f37d50
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-11-22 05:23:08 +0000

    Update tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -12,7 +12,11 @@ cd /home/ubuntu/Jenkins-Repo-Practical-Project
 
 docker-compose up -d
 
+docker exec frontend bash -c "pytest tests/ --cov application" 
+
 docker exec frontend bash -c "pytest tests/ --cov application" | grep passed >> txt
+
+cat txt
 
 some_var=$(cat txt | grep passed )
 
