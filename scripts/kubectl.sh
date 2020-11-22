2dd703071034a591138764436cbd9ef611aa449f
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-11-22 18:20:13 +0000

    Update kubectl.sh

--- scripts/kubectl.sh
+++ scripts/kubectl.sh
@@ -2,4 +2,14 @@ aws eks --region eu-west-1 update-kubeconfig --name project-cluster
 aws s3 ls
 kubectl get svc
 kubectl create namespace project
+kubectl apply -f secrets.yaml 
+kubectl apply -f conf-nginx.yaml 
+kubectl apply -f backend.yaml 
+kubectl apply -f clusterip-backend.yaml 
+kubectl apply -f frontend.yaml 
+kubectl apply -f clusterip-frontend.yaml 
+kubectl apply -f deploy-nginx.yaml 
+kubectl apply -f lb-nginx.yaml 
+
+
 
