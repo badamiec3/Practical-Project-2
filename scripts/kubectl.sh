1b235b073cda83704edc83ebd9798ebb2e3bbee1
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-11-22 20:39:43 +0000

    Update kubectl.sh

--- scripts/kubectl.sh
+++ scripts/kubectl.sh
@@ -2,6 +2,16 @@ aws eks --region eu-west-1 update-kubeconfig --name project-cluster
 aws s3 ls
 kubectl get svc
 cd kubectl/
+
+kubectl delete -f secrets.yaml 
+kubectl delete -f conf-nginx.yaml 
+kubectl delete -f backend.yaml 
+kubectl delete -f clusterip-backend.yaml 
+kubectl delete -f frontend.yaml 
+kubectl delete -f clusterip-frontend.yaml 
+kubectl delete -f deploy-nginx.yaml 
+kubectl delete -f lb-nginx.yaml 
+
 kubectl apply -f secrets.yaml 
 kubectl apply -f conf-nginx.yaml 
 kubectl apply -f backend.yaml 
