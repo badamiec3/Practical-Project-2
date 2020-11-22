018001b49093441bdcb4e8d69ce07474edda99ce
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-11-22 16:30:07 +0000

    Update kubectl-su.sh

--- scripts/kubectl-su.sh
+++ scripts/kubectl-su.sh
@@ -3,8 +3,8 @@
 pwd
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY}
 aws configure set aws_secret_access_key ${AWS_SECRET_KEY}
-sudo su ubuntu'
-whoami'
+sudo su ubuntu
+whoami
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY}
 aws configure set aws_secret_access_key ${AWS_SECRET_KEY}
 sudo su jenkins
