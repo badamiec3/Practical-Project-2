7d9d7322f88b8b6d0d2833474f2e1b2bd7792432
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-11-22 05:46:57 +0000

    Update btests.sh

--- scripts/btests.sh
+++ scripts/btests.sh
@@ -2,5 +2,5 @@
 
 ssh -i /home/jenkins/.ssh/id_rsa ubuntu@10.0.1.18 << EOF
 cd /home/ubuntu/Jenkins-Repo-Practical-Project
-docker exec backend bash -c "pytest tests/ --cov application"  | grep FAILURES
+docker exec backend bash -c "pytest tests/ --cov application"  | grep passed
 EOF
