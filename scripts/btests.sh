b18a42efe3a4327b8a34677161c594a524d0888c
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-11-22 05:36:55 +0000

    Update btests.sh

--- scripts/btests.sh
+++ scripts/btests.sh
@@ -1,14 +1,6 @@
 #! /bin/bash
 
 ssh -i /home/jenkins/.ssh/id_rsa ubuntu@10.0.1.18 << EOF
-cd /home/ubuntu/
-sudo rm -r Jenkins-Repo-Practical-Project
-git clone https://github.com/badamiec3/Jenkins-Repo-Practical-Project.git
 cd /home/ubuntu/Jenkins-Repo-Practical-Project
-docker-compose up -d
-docker exec frontend bash -c "pytest tests/ --cov application" 
-docker exec frontend bash -c "pytest tests/ --cov application" | grep passed >> txt
-cat txt
-some_var=$(cat txt | grep passed)
 docker exec backend bash -c "pytest tests/ --cov application"  | grep passed
 EOF
